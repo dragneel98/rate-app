7294865568851be68811f6be4819c24fbbb2e6a5
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-10-29 00:21:50 -0300

    add login validations

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path")
 const createExpoWebpackConfigAsync = require("@expo/webpack-config")
 
-module.exports = async function (env, arg) {
-    const config = await createExpoWebpackConfigAsync(env, arg)
+module.exports = async function (env, argv) {
+    const config = await createExpoWebpackConfigAsync(env, argv)
 
     config.module.rules.push({
         test: /\.js$/,
         loader: "babel-loader",
-        includes: [
-            path.join(_dirname, "node_modules/react-router-native")
+        include: [
+            path.join(__dirname, "node_modules/react-router-native")
         ]
     })
     return config

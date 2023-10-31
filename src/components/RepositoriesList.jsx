f8d1588eb4c6c135a49bb52eadf830d575123d7e
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-10-30 21:51:03 -0300

    add fetch

--- src/components/RepositoriesList.jsx
+++ src/components/RepositoriesList.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View } from 'react-native';
 import { FlatList } from 'react-native';
 import repositories from '../data/repositories';
 import RepositoryItem from './RepositoryItem';
 
 export default function RepositoriesList() {
+    const [repositories, setRepositories] = useState()
+
+    const getRepo = async () => {
+        const res = await global.fetch("http://localhost:5000/api/repositories")
+        const json = await res.json()
+        setRepositories(json)
+        console.log(json);
+    }
+
+    useEffect(() => {
+        getRepo()
+    }, [])
+
+
     return (
         <FlatList
             data={repositories}
